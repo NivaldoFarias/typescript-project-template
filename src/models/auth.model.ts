4b590992820d6cab21a9d5ead0884a5a704a0c0e
Nivaldo Farias <badivia.f@gmail.com>
2022-08-06 13:18:26 -0300

    chore: change minimum password input length from 10 to 8

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -4,12 +4,12 @@ import { regex } from '../utils/constants.util';
 const register = Joi.object({
   username: Joi.string().pattern(regex.USERNAME).required(),
   email: Joi.string().email().pattern(regex.EMAIL).required(),
-  password: Joi.string().min(10).required(),
+  password: Joi.string().min(8).required(),
 });
 
 const signIn = Joi.object({
   email: Joi.string().email().pattern(regex.EMAIL).required(),
-  password: Joi.string().min(10).required(),
+  password: Joi.string().min(8).required(),
 });
 
 export { register, signIn };
